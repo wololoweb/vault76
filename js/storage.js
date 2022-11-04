afc02177af0a88bb3ed795f2768558aa73aca252
David Sueldo <davidezequielsueldo@gmail.com>
2022-11-04 19:35:39 -0300

    Storage devuelve datos en formulario al ingresar nuevamente al HTML

--- js/storage.js
+++ js/storage.js
@@ -7,11 +7,24 @@ const inputText = document.querySelector("#inputText")
 
 const btnEnviar = document.querySelector("button.button")
 
+
+//////////*  ALMACENAR DATOS *//////////
 const almacenarDatos = () => {
     if(inputNombre.value !== "") {localStorage.setItem("Nombre", inputNombre.value)}
     if(inputApellido.value !== "") {localStorage.setItem("Apellido", inputApellido.value)}
     if(inputEmail.value !== "") {localStorage.setItem("Email", inputEmail.value)}
     if(inputText.value !== "") {localStorage.setItem("Comentario", inputText.value)}
 }
+
+//////////*  RECUPERAR DATOS *//////////
+const recuperarDatos = () => {
+    if(localStorage.getItem("Nombre")){inputNombre.value = localStorage.getItem("Nombre")}
+    if(localStorage.getItem("Apellido")){inputApellido.value = localStorage.getItem("Apellido")}
+    if(localStorage.getItem("Email")){inputEmail.value = localStorage.getItem("Email")}
+    
+}
+
+//////////* CUANDO SE CARGUE EL HTML RECUPERAR DATOS *//////////
+document.addEventListener("DOMContentLoaded", recuperarDatos)
 
 btnEnviar.addEventListener("click", almacenarDatos)
