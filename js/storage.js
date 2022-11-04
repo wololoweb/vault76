4e49087b484272bce623910e2320da5cbc0d7f41
David Sueldo <davidezequielsueldo@gmail.com>
2022-11-04 17:37:51 -0300

    errores storage

--- js/storage.js
+++ js/storage.js
@@ -1,17 +1,17 @@
 
 
-const inputNombre = document.querySelectorAll("#nombre") 
-const inputApellido = document.querySelectorAll("#apellido")
-const inputEmail = document.querySelectorAll("#email")
-const inputText = document.querySelectorAll("#text-area")
+const inputNombre = document.querySelectorAll("#inputNombre") 
+const inputApellido = document.querySelectorAll("#inputApellido")
+const inputEmail = document.querySelectorAll("#inputEmail")
+const inputText = document.querySelectorAll("#inputText")
 
-const btnEnviar = document.querySelector("#enviar")
+const btnEnviar = document.querySelector("button.button")
 
 const almacenarDatos = () => {
-    if(inputNombre.value.trim() !== "") {localStorage.setItem("Nombre", inputNombre.value)}
-    if(inputApellido.value.trim() !== "") {localStorage.setItem("Apellido", inputApellido.value)}
-    if(inputEmail.value.trim() !== "") {localStorage.setItem("Email", inputEmail.value)}
-    if(inputText.value.trim() !== "") {localStorage.setItem("Comentario", inputText.value)}
+    if(inputNombre.value !== "") {localStorage.setItem("Nombre", inputNombre.value)}
+    if(inputApellido.value !== "") {localStorage.setItem("Apellido", inputApellido.value)}
+    if(inputEmail.value !== "") {localStorage.setItem("Email", inputEmail.value)}
+    if(inputText.value !== "") {localStorage.setItem("Comentario", inputText.value)}
 }
 
 btnEnviar.addEventListener("click", almacenarDatos)
